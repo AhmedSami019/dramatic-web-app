73fe543abe4d8b63d437b2fd7e17325f8cc6f1b6
ahmedsami019 <easami019192@gmail.com>
2025-10-31 22:48:40 +0600

    added the carousel component

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
   );
 
   return (
-    <div className="navbar bg-black/40 backdrop-blur-md items-center">
+    <div className="navbar bg-black/40 backdrop-blur-md items-center fixed top-0 left-0 right-0">
       <div className="w-11/12 mx-auto">
         <div className="navbar-start">
           <div className="dropdown">
